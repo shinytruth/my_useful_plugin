04a367ae0493f9b1c1ec77843ebed03abb19e2b1
mjbae <mjbae@runbyme.team>
2023-05-01 13:03:57 +0900

    chore: 변경 테스트

--- ios/Classes/MyUsefulPlugin.swift
+++ ios/Classes/MyUsefulPlugin.swift
@@ -9,6 +9,6 @@ public class MyUsefulPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    result("iOS custom " + UIDevice.current.systemVersion)
   }
 }
